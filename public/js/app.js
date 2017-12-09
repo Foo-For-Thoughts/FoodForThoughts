9b321e0cbcb1c0b7a254ac58fe2a279c29a2c433
Akiyo <flying.unicorn.sf@gmail.com>
2017-12-09 08:29:55 -0800

    small pie chart sort of works

--- public/js/app.js
+++ public/js/app.js
@@ -1,46 +1,22 @@
 $(document).ready(function() {
 
-var userID = undefined;
-var maealData = [];
-  //submit a meal
-  $("#meal-form").on("submit", handleMealDataSumbit);
+// var userID = undefined;
+// var maealData = [];
+//   //submit a meal
+//   $("#meal-form").on("submit", handleMealDataSumbit);
 
-  function handleMealDataSumbit(event){
-    event.preventDefault();
-    $.ajax({
-      method: "POST",
-      url: "/postmeals",
-      data: mealData
-    })
-    .done(function(data) {
-      console.log(data);
-    });
-
-  }; // closing handleMealDataSubmit
-
-// from chart html
-//   $("#defaultOpen").on("click", getUserID);
-//   function getUserID(){
-//     console.log("haha")
-//     $.get("/api/user_data", function(data){
-//         userID = data.id
-//         console.log("userID = " + userID)
-//     })
-//   };
-
-// //THE GET REQUEST URL SHOULD BE /MEALS/DATEVARIABLE/USERID
-//   $(".tablink").on("click", getMealData);
-//   window.getMealData = getMealData;
-//   function getMealData(){
-//     var range = $(this).attr("num");
-//     var path ="/meals/" + range + "/" + userID;
+//   function handleMealDataSumbit(event){
+//     event.preventDefault();
 //     $.ajax({
-//       url: path
-//     }).done(function(data){
-//       console.log(data)
-//       mealData = data
-//   })
-//   }
+//       method: "POST",
+//       url: "/postmeals",
+//       data: mealData
+//     })
+//     .done(function(data) {
+//       console.log(data);
+//     });
+
+//   }; // closing handleMealDataSubmit
 
 
 
