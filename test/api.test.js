279c678305a4602b7cb6faec60237243f3613438
Eneida Revueltas <eneidarevueltas@gmail.com>
2017-12-09 12:32:14 -0800

    pushing to heroku after this

--- test/api.test.js
+++ test/api.test.js
@@ -3,11 +3,13 @@ const db = require('../models/index')
 const expect = require('chai').expect
 const request = require('supertest').agent(app)
 
+console.log(process.env.NODE_ENV)
+
 describe('Some simple API tests', () => {
   describe('A GET to /logout', () => {
     it('should return the index path', done => {
       request
-        .get('/api/random')
+        .get('/logout')
         .expect(200) // check that the status code is a success
         .end((error, response) => {
           expect(response.body.redirectTo).to.equal('/')
@@ -15,39 +17,39 @@ describe('Some simple API tests', () => {
         })
     })
   })
-  // describe('A POST to /api/users', () => {
-  //   before('Delete all users to prevent duplicate key errors', done => {
-  //     db.User.destroy({ truncate: true })
-  //       .then(() => {
-  //         done()
-  //       })
-  //   })
-  //   it('should create and send back a new user record', done => {
-  //     var userAttrs = {
-  //       email: 'davidb.prof@gmail.com',
-  //       password: 'best password ever'
-  //     }
-  //     request
-  //       .post('/api/users')
-  //       .send(userAttrs)
-  //       .end((error, response) => {
-  //         var user = response.body
-  //         // Make sure the password isn't being sent back
-  //         expect(user.password).to.equal(undefined)
-  //         // Make sure the user was created with the email we sent
-  //         expect(user.email).to.equal(userAttrs.email)
-  //         // Make sure the user was created with an id
-  //         expect(user.id).to.not.equal(undefined)
-  //         // Then we'll directly query the database to make sure that the password
-  //         // saved in the databse isn't the plaintext password we sent
-  //         db.User.findOne({ where: { id: user.id }})
-  //           .then( user => {
-  //             expect(user.password).to.not.equal(userAttrs.password)
-  //             // this line of code is really crucial because it tells mocha that 
-  //             // the asynchronous test has finished.
-  //             done()
-  //           })
-  //       })
-  //   })
-  // })
+  describe('A POST to /auth/signup', () => {
+    before('Delete all users to prevent duplicate key errors', done => {
+      db.sequelize.query('TRUNCATE user CASCADE')
+        .then(() => {
+          done()
+        })
+    })
+    it('should create and send back a new user record', done => {
+      var userAttrs = {
+        username: 'erev1',
+        password: 'password'
+      }
+      request
+        .post('/api/users')
+        .send(userAttrs)
+        .end((error, response) => {
+          var user = response.body
+          // Make sure the password isn't being sent back
+          expect(user.password).to.equal(undefined)
+          // Make sure the user was created with the email we sent
+          expect(user.username).to.equal(userAttrs.username)
+          // Make sure the user was created with an id
+          expect(user.id).to.not.equal(undefined)
+          // Then we'll directly query the database to make sure that the password
+          // saved in the databse isn't the plaintext password we sent
+          db.user.findOne({ where: { id: user.id }})
+            .then( user => {
+              expect(user.password).to.not.equal(userAttrs.password)
+              // this line of code is really crucial because it tells mocha that 
+              // the asynchronous test has finished.
+              done()
+            })
+        })
+    })
+  })
 })
